9d658102358df3625099445fad5b821757d69d75
Jonathan Creasy <jonathan@ghostlab.net>
2013-12-17 15:40:45 -0600

    Created device view, added links to /ubersmith/devices

--- app.js
+++ app.js
@@ -317,6 +317,55 @@ app.get('/ubersmith/devices'
     });
   });
 
+// Used to view a single device
+app.get('/ubersmith/devices/:deviceid'
+  , function (req, res) {
+      redisClient.get('device.list', function (err, reply) {
+        if (!reply) {
+          console.log('Sending 500: ' + err);
+          res.send(500);
+        } else {
+          var deviceList = JSON.parse(reply);
+          var uberDevice = deviceList[req.params.deviceid];
+          console.log(uberDevice);
+          var request = require('request');
+          console.log(app.get('sensu_uri') + '/events/' + uberDevice.dev_desc + '.contegix.mgmt');
+          request({ url: app.get('sensu_uri') + '/events/' + uberDevice.dev_desc + '.contegix.mgmt', json: true }
+            , function (error, response, body) {
+                if (!error) {
+                  if (response.statusCode < 300 && body.length > 0)
+                  {
+                    var sensuEvents = body;
+                  } else {
+                    var sensuEvents = [ { output: "No Events Found", status: 0, issued: Date.now(), handlers: [], flapping: false, occurrences: 0, client: uberDevice.dev_desc + '.contegix.mgmt', check: 'N/A'}];
+                  }
+                  console.log(sensuEvents);
+                  console.log(app.get('sensu_uri')+ '/client/' + uberDevice.dev_desc + '.contegix.mgmt')
+                  request({url: app.get('sensu_uri')+ '/client/' + uberDevice.dev_desc + '.contegix.mgmt', json: true}
+                  , function (error, response, body) {
+                      if (!error) {
+                        if (response.statusCode < 300)
+                        {
+                          var sensuClient = body;
+                        } else {
+                          var sensuClient = { address: 'unknown', name: uberDevice.dev_desc, safe_mode: 0, subscriptions: [], timestamp: 0 };
+                        }
+                        console.log(sensuClient);
+                        res.render('ubersmith/device', {device: uberDevice, sensuclient: sensuClient, events: sensuEvents, user:req.user, section: 'devices', navLinks: config.navLinks.ubersmith });
+                      } else {
+                        console.log('Got ' + response.statusCode + ' Sending 500: ' + error);
+                        res.send(500);
+                      }
+                  })
+              } else {
+                console.log('Got ' + response.statusCode + ' Sending 500: ' + error);
+                res.send(500);
+              }
+            })
+        }
+      });
+  });
+
 // Used by device browser, returns table data
 app.get('/ubersmith/devices/list/:devtype_group_id'
   , function (req, res) {
@@ -331,7 +380,7 @@ app.get('/ubersmith/devices/list/:devtype_group_id'
           Object.keys(deviceList).forEach(function(device_id) {
             device = deviceList[device_id];
             if (device.devtype_group_id == req.params.devtype_group_id) {
-              filteredDevice = Array(device.dev, device.type, device.dev_desc, device.company, device.location, device.device_status);
+              filteredDevice = { 'device': device.dev, 'type': device.type, 'desc': device.dev_desc, 'company': device.company, 'location': device.location, 'status': device.device_status};
               aReturn.push(filteredDevice);
               foundSome = true;
             }
