ca5abf50bf4620f7a5fd452f3e8cde99eb386d7b
Jonathan Creasy <jonathan@ghostlab.net>
2014-01-14 21:59:06 -0600

    organized the routes

--- routes/puppet.js
+++ routes/puppet.js
@@ -1,88 +1,20 @@
 module.exports = function (app, config, passport, redisClient) {
-  var request = require('request')
-
-  app.get('/monitoring'
+  app.get('/puppet/facts/:fact'
     , app.locals.requireGroup('users')
     , function (req, res) {
-        request({ url: app.get('sensu_uri') + '/info', json: true }
+      var request = require('request');
+      request({ url: app.get('puppetdb_uri') + '/facts/' + req.params.fact, json: true }
         , function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            res.render('monitoring', {info: body, user:req.user, section: 'info', navLinks: config.navLinks.monitoring });
+          if (error)
+          {
+            app.locals.logger.log('debug', 'failed to get data from PuppetDB', { uri: app.get('puppetdb_uri') + '/facts/' + req.params.fact});
+            res.send(500);
+          } else {
+            app.locals.logger.log('debug', 'fetched data from PuppetDB', { uri: app.get('puppetdb_uri') + '/facts/' + req.params.fact});
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(body));
+            res.end();
           }
         })
-    });
-
-  app.get('/monitoring/events'
-    , app.locals.requireGroup('users')
-    , function (req, res) {
-        request({ url: app.get('sensu_uri') + '/events', json: true }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          res.render('monitoring/events', {events: body, user:req.user, section: 'events', navLinks: config.navLinks.monitoring });
-        }
-      })
-    });
-
-  app.get('/monitoring/stashes'
-    , app.locals.requireGroup('users')
-    , function (req, res) {
-        request({ url: app.get('sensu_uri') + '/stashes', json: true }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          res.render('monitoring/stashes', {stashes: body, user:req.user, section: 'stashes', navLinks: config.navLinks.monitoring });
-        }
-      })
-    });
-
-  app.get('/monitoring/checks'
-    , app.locals.requireGroup('users')
-    , function (req, res) {
-        request({ url: app.get('sensu_uri') + '/checks', json: true }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          res.render('monitoring/checks', {checks: body, user:req.user, section: 'checks', navLinks: config.navLinks.monitoring });
-        }
-      })
-    });
-
-  app.get('/monitoring/failures'
-    , app.locals.requireGroup('users')
-    , function (req, res) {
-        var hoursAgo = 10;
-        request({ url: app.get('puppetdb_uri') + '/events?query=["and", ["=", "status", "failure"],["=", "resource-type", "Service"],[">", "timestamp", "' + moment().subtract('hours', hoursAgo).format() + '"]]]&limit=1000', json: true }, function (error, response, body) {
-//        request({ url: app.get('puppetdb_uri') + '/events?query=["and", ["=", "status", "failure"],["=", "resource-title", "contegix-sensu-client"],["=", "resource-type", "Service"],[">", "timestamp", "' + moment().subtract('hours', hoursAgo).format() + '"]]]&limit=1000', json: true }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          res.render('monitoring/failures', {hoursAgo: hoursAgo, failures: body, user:req.user, section: 'failures', navLinks: config.navLinks.monitoring });
-        }
-      })
-    });
-
-  app.get('/monitoring/clients'
-    , app.locals.requireGroup('users')
-    , function (req, res) {
-        request({ url: app.get('sensu_uri') + '/clients', json: true }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          res.render('monitoring/clients', {clients: body, user:req.user, section: 'clients', navLinks: config.navLinks.monitoring });
-        }
-      })
-    });
-
-  app.get('/monitoring/exceptions'
-    , app.locals.requireGroup('users')
-    , function (req, res) {
-        request({ url: app.get('sensu_uri') + '/clients', json: true }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var sensuClients = body;
-          console.log(sensuClients);
-        }
-      })
-    });
-
-  app.post('/monitoring/stashes/:server'
-    , function (req, res) {
-      var expiration = new Date(oldDateObj.getTime() + 30*60000);
-      request.post({
-        url: app.get('sensu_uri') + '/stashes/silence/' + server,
-        body: "{ 'timestamp': " + Date.now() + ", 'expires': " + expiration + " }"
-      }, function(error, response, body){
-        console.log(body);
-      });
     });
 }
