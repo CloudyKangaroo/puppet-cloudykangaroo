623bfa09949cfc43c5981de474137d010fedbc05
Jonathan Creasy <jonathan@ghostlab.net>
2014-01-14 19:48:28 -0600

    created audit log

--- routes/events.js
+++ routes/events.js
@@ -20,11 +20,11 @@ module.exports = function (app, config, passport, redisClient) {
           app.locals.logger.log('error', 'could not process incoming event', { error: err, event: req.params.event, path: req.path });
           res.send(500);
         }
-	      app.locals.logger.log('info', 'parsing form', { fields: fields, files: files, event: req.params.event });
+	      app.locals.logger.log('debug', 'parsing form', { fields: fields, files: files, event: req.params.event });
       });
 
       form.on('field', function(name, value) {
-        app.locals.logger.log('info', 'event field received', { field: name, event: req.params.event, value: value });
+        app.locals.logger.log('debug', 'event field received', { field: name, event: req.params.event, value: value });
         fields[name] = value;
       });
 
@@ -39,6 +39,9 @@ module.exports = function (app, config, passport, redisClient) {
 
       form.on('end', function() {
         app.locals.logger.log('debug', 'event complete', { event: req.params.event, fields: fields});
+        var ctxlog = require('../../ctxlog');
+        var auditLog = ctxlog('audit', 'info');
+        auditLog.log('info', req.path, { event: req.params.event, fields: fields})
         // 202 Accepted
         res.send(202);
       });
