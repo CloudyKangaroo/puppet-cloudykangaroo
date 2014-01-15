7b48bd33ea9766abc0d23bb9590b2b7fa73a487b
Jonathan Creasy <jonathan@ghostlab.net>
2014-01-15 16:15:39 -0600

    improved ticket view, added puppet functionality and customer linkage

--- routes/puppet.js
+++ routes/puppet.js
@@ -17,4 +17,59 @@ module.exports = function (app, config, passport, redisClient) {
           }
         })
     });
+  app.get('/puppet/nodes/:node'
+    , app.locals.requireGroup('users')
+    , function (req, res) {
+      var request = require('request');
+      request({ url: app.get('puppetdb_uri') + '/nodes/' + req.params.node, json: true }
+        , function (error, response, body) {
+          if (error)
+          {
+            app.locals.logger.log('debug', 'failed to get data from PuppetDB', { uri: app.get('puppetdb_uri') + '/facts/' + req.params.node});
+            res.send(500);
+          } else {
+            app.locals.logger.log('debug', 'fetched data from PuppetDB', { uri: app.get('puppetdb_uri') + '/nodes/' + req.params.node + '/facts'});
+            var puppetNode = body;
+            puppetNode.catalog_time = app.locals.getFormattedISO8601(puppetNode.catalog_timestamp);
+            puppetNode.facts_time = app.locals.getFormattedISO8601(puppetNode.facts_timestamp);
+            puppetNode.report_time = app.locals.getFormattedISO8601(puppetNode.report_timestamp);
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify({ aaData: body}));
+            res.end();
+          }
+        })
+    });
+
+  app.get('/puppet/facts/mactohosts/:macaddress'
+    , app.locals.requireGroup('users')
+    , function (req, res) {
+      var request = require('request');
+      request({ url: app.get('puppetdb_uri') + '/facts/macaddress', json: true}
+        , function (error, response, body) {
+          app.locals.logger.log('debug', 'fetched data from PuppetDB', { uri: app.get('puppetdb_uri') + '/facts/macaddress'});
+          var facts = body;
+          for (var i = 0; i<facts.length; i++)
+          {
+              if (facts[i].value = req.params.macaddress)
+              {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.write(JSON.stringify({ hostname: facts[i].certname }));
+                res.end();
+              }
+          }
+        });
+    });
+
+  app.get('/puppet/nodes/:node/facts'
+    , app.locals.requireGroup('users')
+    , function (req, res) {
+      var request = require('request');
+      request({ url: app.get('puppetdb_uri') + '/nodes/' + req.params.node + '/facts', json: true}
+        , function (error, response, body) {
+          app.locals.logger.log('debug', 'fetched data from PuppetDB', { uri: app.get('puppetdb_uri') + '/nodes/' + req.params.node + '/facts'});
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.write(JSON.stringify({ aaData: body }));
+          res.end();
+          });
+      });
 }
