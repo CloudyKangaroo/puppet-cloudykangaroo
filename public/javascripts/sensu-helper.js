e5580a4eb5b2880479a526a9c6ca004d0c070c75
Jonathan Creasy <jonathan@ghostlab.net>
2014-01-31 15:35:34 -0600

    add event to ticket, form done

--- public/javascripts/sensu-helper.js
+++ public/javascripts/sensu-helper.js
@@ -31,6 +31,35 @@ var displayTextAlert = function(text)
   bootbox.alert('<pre>' + syntaxHighlight(decodeURI(text)) + '</pre>');
 }
 
+var displayEventDetails = function(text)
+{
+  //http://note19.com/2007/05/27/javascript-guid-generator/
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+
+  var html = '<container><div class="row"><div class="col-md-6"><pre>' + syntaxHighlight(decodeURI(text)) + '</pre></div><div class="col-md-4"><div class="row">';
+  html +=  '<form role="form" id="' + uuid + '-form">' +
+    '<div class="form-group">' +
+    '<fieldset><legend>Attach this event to a Ticket</legend>' +
+    '<label for="ticketID">Enter an existing Ticket number:</label>' +
+    '<input type="email" class="form-control" id="' + uuid + '-ticketID" placeholder="Enter a valid, existing, Ticket Number">' +
+    '<label>Or:&nbsp;<input id="escalate-' + uuid + '" type="checkbox">&nbsp;Escalate to a NEW ticket</label></div>' +
+    '</fieldset>' +
+    '</div><div class="row"><fieldset><legend>Pass along any relevant documentation: </legend><div class="form-group">' +
+    '<textarea class="form-control" rows="3"></textarea>' +
+    '<label for="exampleInputFile">File input</label>' +
+    '<input type="file" disabled id="exampleInputFile">' +
+    '<p class="help-block">File size limit 10MB.</p>' +
+    '</fieldset>' +
+    '<input type="hidden" value="' + uuid + '" id="uuid" name="uuid">' +
+    '<button type="submit" class="btn btn-default">Submit Ticket</button>' +
+    '</form>' +
+    '<script type="text/javascript">$("#escalate-' + uuid + '").click(function() { $("#' + uuid + '-ticketID").attr("disabled", this.checked); }); $( "#' + uuid + '-form" ).submit(function( event ) { alert( "Handler for .submit() called." ); event.preventDefault(); })</script></div></div></div>';
+  bootbox.alert({"message": html, "className" : "wide-bootbox"});
+};
+
 function syntaxHighlight(json) {
   if (typeof json != 'string') {
     json = JSON.stringify(json, undefined, 2);
