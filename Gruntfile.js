8c0ed79e7551a5b304ec15efee89ff159d20060a
Jonathan Creasy <jonathan@ghostlab.net>
2014-03-12 17:08:38 -0500

    added coveralls support

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,14 @@ module.exports = function(grunt) {
         'pre-commit': 'test',
       }
     },
+    coveralls: {
+      options: {
+        force: true
+      },
+      coveralls: {
+        src: 'lcov.info'
+      }
+    },
     jshint: {
       all: ['Gruntfile.js', 'src/app.js', 'src/routes/**/*.js', 'src/lib/**/*.js', 'src/public/javasripts/newTicketWizard.js', 'src/public/javascripts/sensu-helper.js', 'src/public/javascripts/puppet_gauges.js' ]
     },
@@ -20,6 +28,17 @@ module.exports = function(grunt) {
       test: {
         options: {
           reporter: 'travis-cov',
+          files: 'test/**/*.js',
+          src: ['test/**/*.js']
+        },
+        'pattern': [
+          'src'
+        ],
+        'data-cover-never': 'node_modules'
+      },
+      lcov: {
+        options: {
+          reporter: 'mocha-lcov-reporter',
           files: 'test/**/*.js',
           src: ['test/**/*.js']
         },
@@ -45,8 +64,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-mocha-cov');
+  grunt.loadNpmTasks('grunt-coveralls');
 
   grunt.registerTask('hooks', 'githooks');
   grunt.registerTask('test', ['env:test', 'mochaTest', 'mochacov:test', 'jshint']);
+  grunt.registerTask('report', ['mochacov:lcov', 'coveralls']);
   grunt.registerTask('default', ['env:development', 'mochaTest']);
 };
