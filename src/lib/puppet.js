b48b602e095e77dbee0cba1da4eeafa337cea570
Jonathan Creasy <jonathan@ghostlab.net>
2014-04-17 14:50:08 -0500

    fixed issue in puppet library

--- src/lib/puppet.js
+++ src/lib/puppet.js
@@ -35,8 +35,13 @@ module.exports = function(config, logger) {
     var request = require('request');
     var url = config.puppetdb.uri + '/nodes/' + hostname;
     request({ url: url, json: true }, function (error, response) {
-      logger.log('verbose', 'response from puppet', {body: response.body, url: url});
-      callback(error, response.body);
+      if (error) {
+        callback(error);
+      } else {
+        console.log(response);
+        logger.log('verbose', 'response from puppet', {body: response.body, url: url});
+        callback(null, response.body);
+      }
     });
   };
 
@@ -53,38 +58,33 @@ module.exports = function(config, logger) {
     });
   };
 
-  var handlePuppetDBNode = function(hostname, err, results, callback) {
-    if (err)
+  var handlePuppetDBNode = function(hostname, results, callback) {
+    if (results && results.length === 2)
     {
-      callback(err);
-    } else {
-      if (results && results.length === 2)
+      var puppetDevice = {};
+      if (results[0].error)
       {
-        var puppetDevice = {};
-        if (results[0].error)
-        {
-          var node = {
-            name: hostname,
-            deactivated: null,
-            catalog_timestamp: '2014-01-22T04:11:05.562Z',
-            facts_timestamp: '2014-01-22T04:10:58.232Z',
-            report_timestamp: '2014-01-22T04:11:04.076Z'
-          };
-          puppetDevice = {error: results[0].error, node: node, facts: []};
-        } else {
-          var facts = results[1];
-          var factInfo = {};
-          for (var i=0; i<facts.length; i++)
-          {
-            var fact = facts[i];
-            factInfo[fact.name] = fact.value;
-          }
-          puppetDevice = {node: results[0], facts: factInfo, factsArray: facts};
-        }
-        callback(null, puppetDevice);
+        var node = {
+          name: hostname,
+          deactivated: null,
+          catalog_timestamp: '2014-01-22T04:11:05.562Z',
+          facts_timestamp: '2014-01-22T04:10:58.232Z',
+          report_timestamp: '2014-01-22T04:11:04.076Z'
+        };
+        puppetDevice = {error: results[0].error, node: node, facts: []};
       } else {
-        callback(new Error('could not retrieve host and facts from Puppet'));
+        var facts = results[1];
+        var factInfo = {};
+        for (var i=0; i<facts.length; i++)
+        {
+          var fact = facts[i];
+          factInfo[fact.name] = fact.value;
+        }
+        puppetDevice = {node: results[0], facts: factInfo, factsArray: facts};
       }
+      callback(null, puppetDevice);
+    } else {
+      callback(new Error('could not retrieve host and facts from Puppet'));
     }
   };
 
@@ -99,7 +99,11 @@ module.exports = function(config, logger) {
         getPuppetDBNodeFacts(hostname, callback);
       }
     ], function (err, results) {
-      handlePuppetDBNode(hostname, err, results, getDevCallback);
+      if (err) {
+        getDevCallback(err);
+      } else {
+        handlePuppetDBNode(hostname, results, getDevCallback);
+      }
     });
   };
 
