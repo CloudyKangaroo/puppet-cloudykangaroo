f0040b460e5f7f41f78f9925fcc118588ac1affe
Jonathan Creasy <jonathan@ghostlab.net>
2014-09-14 17:50:18 -0700

    Removed start script, updated Gruntfile to include "run" command

--- start.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-#sudo /opt/boxen/homebrew/bin/redis-server /opt/boxen/config/redis/redis.conf;
-REDIS_CLASS="fakeredis" CREDS_CLASS="./config/system-dev-credentials" MGMT_DOMAIN=".unittest.us" MON_CLASS="./lib/mockMonitoring" CRM_CLASS="cloudy-localsmith" USE_NOCK=true NODE_ENV=development PORT=3001 node src/app.js
